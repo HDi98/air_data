87cb08469c8d4e35af44ed45869f9f6f37fa6bc9
Haonan Di <Haonan_Di@163.com>
2020-11-18 11:22:51 +0800

    优化提数逻辑

--- air_new/most_delay.sql
+++ air_new/most_delay.sql
@@ -1,5 +1,5 @@
 --延误次数最多的航班
-select airdata_year.* from airdata.airdata_year t0
+select * from airdata.airdata_year t0
 
 inner join
 (
@@ -9,14 +9,22 @@ inner join
     group by origin, dest 
     order by delay_count DESC
     limit 1
-)t1 
-on t0.origin = t1.origin, 
-t0.dest = t1.dest
+) t1 
+on t0.origin = t1.origin
+and t0.dest = t1.dest
 
 left join 
-airdata.weather t2
+airdata.weather dep
 on 
-t2.yeartmp = t0.year,
-t2.monthtmp = t0.month,
-t2.daytmp = t0.dayofmonth,
-t2.airporttmp = t0.origin
+dep.yeartmp = t0.year
+and dep.monthtmp = t0.month
+and dep.daytmp = t0.dayofmonth
+and dep.airporttmp = t0.origin
+
+left join 
+airdata.weather arr 
+on 
+arr.yeartmp = t0.year
+and arr.monthtmp = t0.month
+and arr.daytmp = t0.dayofmonth
+and arr.airporttmp = t0.dest
